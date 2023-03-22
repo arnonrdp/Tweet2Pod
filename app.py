e6604fe0ecc3383c073a63997585f3f8c5f4414f
Arthur <arthurdesosuafernandes@gmail.com>
2023-03-22 14:30:08 -0300

    feat: Função de Download do Vídeo e da capa

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 from post_anchor import anchor_connect
 from search_tweet import twitter_connect
 
+
+
 if __name__ == '__main__':
     load_dotenv()
     anchor_connect()
+    twitter_connect()
